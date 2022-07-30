9d59b34a07fe1c2eca19935ca5071b01c625096b
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2022-07-31 02:19:18 +0900

    add(Basic Structure) (#3)
    #1
    
    + 기본 프로젝트 생성

---
+++ src/containers/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Footer } from '../commons/Footer';
+import { Header } from '../commons/Header';
+import Pages from "../pages";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={
+          <Pages.Index />} />
+        <Route path='/map' element={
+          <Pages.Map />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
