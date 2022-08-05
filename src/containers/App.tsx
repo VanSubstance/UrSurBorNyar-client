12f74e69b21e611650d4aea292b9fc2d774e22d9
MJ <75807617+kang123077@users.noreply.github.com>
2022-08-06 00:44:47 +0900

    add(Basic Modal System with Recoil)
    #6
    + recoil
    + pretendard font
    + basic modal system

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Footer } from '../commons/Footer';
-import { Header } from '../commons/Header';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import { Footer } from "../commons/Footer";
+import { Header } from "../commons/Header";
 import Pages from "../pages";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/' element={
-          <Pages.Index />} />
-        <Route path='maps'>
-          <Route path='kakao' element={<Pages.Maps.Kakao />} />
-          <Route path='naver' element={<Pages.Maps.Naver />} />
-        </Route>
-        <Route path='*' element={<Pages.NotFound />} />
-      </Routes>
-      <Footer />
+      <RecoilRoot>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Pages.Index />} />
+          <Route path="maps">
+            <Route path="kakao" element={<Pages.Maps.Kakao />} />
+            <Route path="naver" element={<Pages.Maps.Naver />} />
+          </Route>
+          <Route path="*" element={<Pages.NotFound />} />
+        </Routes>
+        <Footer />
+      </RecoilRoot>
     </BrowserRouter>
   );
 }
