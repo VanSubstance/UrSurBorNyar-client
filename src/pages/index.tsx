eca347d0b4b3412be4b7b897612e60d9a2462a41
MJ <75807617+kang123077@users.noreply.github.com>
2022-08-06 00:44:47 +0900

    add(Basic Modal System with Recoil)
    #6
    + recoil
    + pretendard font
    + basic modal system

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,27 @@
 import { memo } from "react";
 import NotFound from "./404";
 import Map from "./maps";
+import { useRecoilState } from "recoil";
+import ModalVisibility from "../recoils/modalvisibility";
+import { useEffect } from "react";
+import ModalContent from "../components/ModalContent";
 
 const Index = () => {
-  return (
-    <div>
-      메인-페이지ㅡ!
-    </div>
-  )
-}
+  const [modalState, setModalState] = useRecoilState(ModalVisibility);
+  const openModal = () => {
+    setModalState({
+      type: "start",
+      children: "모임 인원을 입력해주세요@@!!",
+    });
+  };
+
+  useEffect(() => {
+    openModal();
+    console.log(modalState.type);
+  }, []);
+
+  return <div>메인-페이지ㅡ!<ModalContent/></div>;
+};
 
 export default {
   Index: memo(Index),
