c4965126670c5d6edb992148cd22447b3e5ee62b
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2022-08-16 17:07:51 +0900

    fix(QA)
    #7
    
    ~ 병합 후 발생한 버그 수정
    ~ 메인 페이지 쪽으로 병합
    - 불필요한 컴포넌트 삭제
    
    @kang123077 코드 팔로잉 필수

--- src/components/imports/Map.tsx
+++ src/components/imports/Map.tsx
@@ -2,7 +2,7 @@ import React, { memo, useCallback, useMemo, useState } from 'react';
 import { useEffect } from 'react';
 import styled from 'styled-components';
 
-export const KakaoMap = memo(({ width = `500px`, height = `400px` }: { width?: string; height?: string; }) => {
+export const KakaoMap = memo(({ width = `500px`, height = `400px` }: sizeProps) => {
   const [mapInstance, setMapInstance] = useState(null);
   const [centerCoor, setCenterCoor] = useState<coorsProps>({
     lat: 37.5666805,
@@ -44,56 +44,17 @@ export const KakaoMap = memo(({ width = `500px`, height = `400px` }: { width?: s
   );
 });
 
-export const NaverMap = memo(({ width = `500px`, height = `400px` }: { width?: string; height?: string; }) => {
-  const [mapInstance, setMapInstance] = useState(null);
-  const [centerCoor, setCenterCoor] = useState<coorsProps>({
-    lat: 37.5666805,
-    lng: 126.9784147
-  });
-
-  const getNaverCoors = useCallback(({ lat, lng }: coorsProps) => {
-    return new window.naver.maps.LatLng(lat, lng);
-  }, [window.naver]);
-
-  useEffect(() => {
-    if ('geolocation' in navigator) {
-      navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
-        setCenterCoor({
-          lat: latitude,
-          lng: longitude,
-        });
-      });
-    }
-  }, [navigator]);
-
-  useEffect(() => {
-    const map = new window.naver.maps.Map('map', {
-      center: getNaverCoors(centerCoor),
-      level: 3,
-    });
-    setMapInstance(map);
-  }, []);
-
-  useEffect(() => {
-    if (mapInstance) {
-      mapInstance.setCenter(getNaverCoors(centerCoor));
-    }
-  }, [centerCoor]);
-
-  return (
-    <MapWrapper id={`map`} width={width} height={height}></MapWrapper>
-  );
-});
-
 export interface coorsProps {
   lat: number;
   lng: number;
 }
 
-const MapWrapper = styled.div<{
+interface sizeProps {
   width?: string;
   height?: string;
-}>`
+}
+
+const MapWrapper = styled.div<sizeProps>`
 ${({ width, height }) => {
     return `
       width: ${width};
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -3,25 +3,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import { Footer } from "../commons/Footer";
 import { Header } from "../commons/Header";
+import ModalContent from "../components/ModalContent";
 import Pages from "../pages";
 
 function App() {
   return (
-      <RecoilRoot>
-    <BrowserRouter>
-      {/* <Header /> */}
-      <Routes>
-        <Route path='/' element={
-          <Pages.Maps.Kakao />} />
-        <Route path='maps'>
-          <Route path='kakao' element={<Pages.Maps.Kakao />} />
-          <Route path='naver' element={<Pages.Maps.Naver />} />
-        </Route>
-        <Route path='*' element={<Pages.NotFound />} />
-      </Routes>
-      {/* <Footer /> */}
-    </BrowserRouter>
-      </RecoilRoot>
+    <RecoilRoot>
+      <BrowserRouter>
+        {/* <Header /> */}
+        <ModalContent />
+        <Routes>
+          <Route path='/' element={
+            <Pages.Index />} />
+          <Route path='*' element={<Pages.NotFound />} />
+        </Routes>
+        {/* <Footer /> */}
+      </BrowserRouter>
+    </RecoilRoot>
   );
 }
 
